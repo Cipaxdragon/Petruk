1e0bada74185bd85eb85bd812982b57a32d1898e
Asep <bikingimil@gmail.com>
2022-04-30 10:23:29 +0800

    fixeddd

--- MID/60900121064_AhmadGhazali_C.cpp
+++ MID/60900121064_AhmadGhazali_C.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 using namespace std;
+void proses(int a,int b,int c);
 int main(){
     char yn;
     int a,b,c;
@@ -10,24 +11,7 @@ int main(){
     cout << "Input B = "; cin >> b;
     cout << "Input C = "; cin >> c;
     
-    for(int i = 1;i <= 100;i++){
-        if(i % 2 == 0 ){
-            if(i > b and i < c){
-                cout << "Bilangan Genap yang Lebih dari " << b << " dan kurang dari " << c << " == " << i << endl;
-            }
-        }
-    }
-
-    cout << endl;
-
-    for(int i = 1;i <= 100;i++){
-        if(i % a == 0){
-            if(i > b and i < c){
-                cout << "Bilangan Kelipatan " << a << " Yang Lebih Besar dari " << b << " dan lebih kecil dari " << c << " == " << i << endl;
-            }
-        }
-    }
-
+    proses(a,b,c);
     while (true){        
         cout << endl;
         cout << "Restart the program? [y/n]==> ";
@@ -43,4 +27,31 @@ int main(){
         }
     }
     goto ulang;
+}
+
+
+void proses(int a,int b,int c){
+    for(int j = 1;j <= 1;j++){
+        if(b > c){
+                cout << "B tidak boleh lebih besar dari c" << endl;
+                continue;
+        }
+        for(int i = 1;i <= 100;i++){
+            if(i % 2 == 0 ){
+                if(i > b and i < c){
+                    cout << "Bilangan Genap yang Lebih dari " << b << " dan kurang dari " << c << " == " << i << endl;
+                }
+            }
+        }
+
+        cout << endl;
+
+        for(int i = 1;i <= 100;i++){
+            if(i % a == 0){
+                if(i > b and i < c){
+                    cout << "Bilangan Kelipatan " << a << " Yang Lebih Besar dari " << b << " dan lebih kecil dari " << c << " == " << i << endl;
+                }
+            }
+        }
+    }
 }
