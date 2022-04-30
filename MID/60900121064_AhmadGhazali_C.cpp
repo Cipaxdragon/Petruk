290ac42be79d6d06451402c0f9545c8e597339c5
Asep <bikingimil@gmail.com>
2022-04-30 10:13:52 +0800

    fixed

--- MID/60900121064_AhmadGhazali_C.cpp
+++ MID/60900121064_AhmadGhazali_C.cpp
@@ -1,67 +1,46 @@
 #include <iostream>
 using namespace std;
-string positif_negatif(int &number){
-	if(number >= 0){
-		return "Positif";
-	}else{
-		return "Negatif";
-	}
-}
-string ganjil_genap(int &number){
-	if(number % 2 == 0){
-		return "genap";
-	}else{
-		return "ganjil";
-	}
-}
-
-void kelipatan(int number){
-	if(number % 3 == 0){
-		cout << "Ini Kelipatan 3" << endl;
-	}
-	else if(number % 4 == 0){
-		cout << "Ini Kelipatan 4" << endl;
-	}
-	else if(number % 5 == 0){
-		cout << "Ini Kelipatan 5" << endl;
-	}
-	else if(number % 7 == 0){
-		cout << "Ini Kelipatan 7" << endl;
-	}
-	else if(number % 9 == 0){
-		cout << "Ini Kelipatan 9" << endl;
-	}else{
-		cout << "Ini Bukan Kelipatan 3,4,5,7 atau 9" << endl;
-	}
-}
-
 int main(){
     char yn;
-    int number;
+    int a,b,c;
     ulang : 
-        cout << "==== Program  =====" << endl;
+    cout << "===== Program  ======" << endl;
 
-        cout << "[input]==> ";
-        cin >> number;
-
-        cout << "Bilangan ini adalah bilangan " << positif_negatif(number) << endl;
-
-        cout << "Bilangan ini adalah bilangan " << ganjil_genap(number) << endl;
-        kelipatan(number);
-
-        while (true){        
-            cout << endl;
-            cout << "Restart the program? [y/n]==> ";
-            cin >> yn;
-            if(yn == 'y'){
-                break;
-            }else if(yn == 'n'){
-                cout << endl << "exiting........" << endl;
-                system("pause");
-                return 0;
-            }else{
-                cout << "Inputan Cuma [y/n] (Huruf Kecil)" << endl;
+    cout << "Input A = "; cin >> a;
+    cout << "Input B = "; cin >> b;
+    cout << "Input C = "; cin >> c;
+    
+    for(int i = 1;i <= 100;i++){
+        if(i % 2 == 0 ){
+            if(i > b and i < c){
+                cout << "Bilangan Genap yang Lebih dari " << b << " dan kurang dari " << c << " == " << i << endl;
             }
         }
+    }
+
+    cout << endl;
+
+    for(int i = 1;i <= 100;i++){
+        if(i % a == 0){
+            if(i > b and i < c){
+                cout << "Bilangan Kelipatan " << a << " Yang Lebih Besar dari " << b << " dan lebih kecil dari " << c << " == " << i << endl;
+            }
+        }
+    }
+
+    while (true){        
+        cout << endl;
+        cout << "Restart the program? [y/n]==> ";
+        cin >> yn;
+        if(yn == 'y'){
+            break;
+        }else if(yn == 'n'){
+            cout << endl << "exiting........" << endl;
+            system("pause");
+            return 0;
+        }else{
+            cout << "Inputan Cuma [y/n] (Huruf Kecil)" << endl;
+        }
+    }
     goto ulang;
 }
